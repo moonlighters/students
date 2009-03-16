d510c1e8a82f1b104d8c732f6c3bddd503c16fcb
Vladimir Parfinenko <parfinenko.vladimir@gmail.com>
2009-03-16 22:28:46 +0600

    Add model for Forum Tree

---
+++ app/models/forum.rb
@@ -0,0 +1,6 @@
+class Forum < ActiveRecord::Base
+  validates_presence_of :title, :description
+
+  acts_as_tree
+                      
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090203162225) do
+ActiveRecord::Schema.define(:version => 20090316160327) do
+
+  create_table "forums", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
