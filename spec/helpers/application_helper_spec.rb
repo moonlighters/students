84388899d1dd457f197f4fd0294931cc459a4692
Vladimir Parfinenko <parfinenko.vladimir@gmail.com>
2009-03-25 00:38:21 +0600

    More specs for add_br_to_text

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,8 +4,13 @@ describe ApplicationHelper do
   include ApplicationHelper
   describe "#add_br_to_text" do
     it "should add a <br/> tag to every newline character in text" do
-      add_br_to_text( "aaaad" ).should == "aaaad"
       add_br_to_text( "a\naaa\n\nd" ).should == "a<br/>\naaa<br/>\n<br/>\nd"
+    end
+
+    it "should not change text without newline characters" do
+      ["aaa", "aa\taa", "", "bb\r"].each do |s|
+        add_br_to_text( s ).should == s
+      end
     end
   end
 end
