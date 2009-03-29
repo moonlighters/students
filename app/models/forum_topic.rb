331ec7b5dd6ba55cbac07c20896093c11b8d3100
NIA <nia.fti@mail.ru>
2009-03-29 18:27:38 +0700

    Added ForumTopicViewing model, without specs yet

--- app/models/forum_topic.rb
+++ app/models/forum_topic.rb
@@ -7,11 +7,24 @@ class ForumTopic < ActiveRecord::Base
   has_many  :posts,
             :class_name => "ForumPost"
 
+  has_many  :viewings,
+            :class_name => "ForumTopicViewing"
+  has_many  :readers,
+            :through => :viewings,
+            :source => :user
+
   before_create do |topic|
     topic.view_count = 0
   end
 
-  def view!
+  def view!( user )
+    if user
+      v = ForumTopicViewing.find_or_initialize_by_user_id_and_forum_topic_id( user.id, self.id )
+      v.post = ForumPost.find :first,
+                              :conditions => ["forum_topic_id = ?", self.id],
+                              :order => "id DESC"
+      v.save
+    end
     self.view_count += 1
     self.save
   end
