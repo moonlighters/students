adb7202eccf6e0e155850b4ea30a3776c57ece46
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-07-23 16:28:03 +0700

    Fixes in forum views

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -1,9 +1,10 @@
 module ForumsHelper
   def link_to_forum(forum, options = {})
+    caption = options.delete :caption
     if forum
-      link_to h(forum.title), forum_path( forum ), options
+      link_to caption || h(forum.title), forum_path( forum ), options
     else
-      link_to "Форумы", forums_path, options
+      link_to caption || "Форумы", forums_path, options
     end
   end
 
