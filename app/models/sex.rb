6ff484d20c09dd53aac5b54d3a3b41f224c46f2a
NIA <nia.fti@mail.ru>
2009-07-26 19:29:13 +0400

    Fixed error in Sex's validation regexp, which prevented migrating

--- app/models/sex.rb
+++ app/models/sex.rb
@@ -2,7 +2,7 @@ class Sex < ActiveRecord::Base
   has_many :users
   validates_presence_of :name
   validates_uniqueness_of :name
-  validates_format_of :name, :with => /^[мужской|женский|не определился]$/
+  validates_format_of :name, :with => /^(?:мужской|женский|не определился)$/
 
   def self.male; Sex.find_by_name "мужской"; end
   def self.female; Sex.find_by_name "женский"; end
