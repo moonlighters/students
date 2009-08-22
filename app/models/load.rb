655a2528d9e923b41244ccae1a0a61bfc55b96d1
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-08-23 01:08:30 +0700

    Load now acts_as_ownable

--- app/models/load.rb
+++ app/models/load.rb
@@ -1,25 +1,12 @@
 class Load < ActiveRecord::Base
 
   acts_as_authorization_object
+  acts_as_ownable
 
   # TODO: hide file with path and url options
   has_attached_file :file
 
-  # TODO: acts_as_taggable
-  
   validates_presence_of :name
-  validates_presence_of :owner
   validates_attachment_presence :file, :message => "должен присутствовать"
 
-  def owner
-    unless accepted_roles.first.nil?
-      accepted_roles.first.users.first
-    end
-  end
-
-  def owner=(user)
-    accepts_role!( :owner, user ) if user
-  end
-
-  def owner?(u); u == owner; end
 end
