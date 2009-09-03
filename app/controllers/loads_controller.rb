6390f467684212d5a72d9a4f335b5f71d2528425
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-09-03 23:35:29 +0700

    Rollback of rollback...

--- app/controllers/loads_controller.rb
+++ app/controllers/loads_controller.rb
@@ -1,8 +1,10 @@
 class LoadsController < InheritedResources::Base
 
+  before_filter :find_load, :only => [:edit, :update, :destroy]
+
   access_control do
     allow all, :to => [:index, :show]
-    allow :owner, :of => :resource, :to => [:edit, :update, :destroy]
+    allow :owner, :of => :load, :to => [:edit, :update, :destroy]
     allow :upload_moderator, :to => [:new, :create]
   end
 
@@ -31,6 +33,9 @@ class LoadsController < InheritedResources::Base
   end
 
   private
+
+    def find_load; resource; end
+
     # Override these 2 methods in the particular controller to change behavior when access denied
     def access_denied_redirect_url
       if %w{edit update destroy}.include? action_name
