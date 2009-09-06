d3a0a59f22f89fee6d60a61334e370f6ac8bd43e
NIA <nia.fti@mail.ru>
2009-09-06 20:02:24 +0700

    Added pagination and multiple-tags-filtering to Loads

--- spec/models/load_spec.rb
+++ spec/models/load_spec.rb
@@ -36,4 +36,29 @@ describe Load do
       Tag.find_by_name( "two" ).should be_nil
     end
   end
+
+  describe ".paginate_tagged_with" do
+    before do
+      @tag_sets = %w{ a a,b b,c,d c a,c,d b,a,d,c f,c }.reverse
+      @loads = @tag_sets.map { |tags| Factory :load, :tag_list => tags }
+      @l = @loads.reverse
+    end
+
+    it "should return last loads given no tag" do
+      Load.paginate_tagged_with( [], :page => 1, :per_page => 3).should == @loads.reverse[0..2]
+      Load.paginate_tagged_with( [], :page => 2, :per_page => 3).should == @loads.reverse[3..5]
+    end
+    it "should return last loads filtered by given one tag" do
+      tag = Tag.find_by_name! "a"
+      Load.paginate_tagged_with( [tag], :page => 1, :per_page => 3).should == [@l[0], @l[1], @l[4]]
+      Load.paginate_tagged_with( [tag], :page => 2, :per_page => 3).should == [@l[5]]
+    end
+    it "should return last loads given filtered by multiple tags" do
+      tags = [Tag.find_by_name!( "b" ),
+              Tag.find_by_name!( "c" ),
+              Tag.find_by_name!( "d" )]
+      Load.paginate_tagged_with( tags, :page => 1, :per_page => 1).should == [@l[2]]
+      Load.paginate_tagged_with( tags, :page => 2, :per_page => 1).should == [@l[5]]
+    end
+  end
 end 
