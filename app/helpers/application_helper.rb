98ad93bf67326c76c3144a6fd62ecca56b2f7a57
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-11-01 01:44:50 +0600

    Упростил time_format
    Покрайней мере мне стало понятно что происходит и убрано дублирование

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,29 +12,37 @@ module ApplicationHelper
     messages
   end
 
-  def format_time(time, options={})
-    # set default values for :date, :time and :year options
-    [:date, :time, :year].each {|param| options[param] = true unless options.include? param }
+  def format_time(datetime, options={})
+    date = options[:date] != false
+    year = options[:year] != false
+    time = options[:time] != false
+    ansi = options[:format] == :ansi
+    digit_month = options[:month] == :digits
 
-    # check if either time or date will be shown
-    raise ArgumentError, "Invalid format_time options" if not options[:time] and not options[:date]
+    raise ArgumentError, "Invalid format_time options" unless date or time
 
-    delimiter = options[:month] == :digits ? "." : " "
-    month_format = delimiter + ( options[:month] == :digits ? "%m" : "%B")
-    time_format = options[:time] ? "%H:%M" : ""
-    if options[:date]
-      year_format = options[:year] ? delimiter + "%Y" : ""
-      date_format = options[:format] == :ansi ? "%Y-%m-%d" : "%e#{month_format}" + year_format
-    else
-      date_format = ""
+    time_format = ( "%H:%M" if time )
+
+    date_format = if date
+      if ansi
+        "%Y-%m-%d"
+      else
+        [ "%e",
+          digit_month ? "%m" : "%B",
+          ( "%Y" if year )
+        ].select {|x| !x.nil?}.join( digit_month ? "." : " ")
+      end
     end
-    if options[:format] == :ansi
-      format = ( options[:time] and options[:date] ) ? date_format + " " + time_format : date_format + time_format
+
+    format = if ansi
+      [date_format, time_format].select {|x| !x.nil?}.join " "
     else
-      format = ( options[:time] and options[:date] ) ? time_format + ", " + date_format : time_format + date_format
+      [time_format, date_format].select {|x| !x.nil?}.join ", "
     end
-    Russian::strftime time, format
+
+    Russian::strftime datetime, format
   end
+
   # A wrapper:
   def ansi_date(time)
     format_time time, :time => false, :format => :ansi
