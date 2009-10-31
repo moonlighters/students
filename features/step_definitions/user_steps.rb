9650e9609fde52988e2832e43d7369fb7bc25d3f
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-11-01 00:40:37 +0600

    Остатки переименовывания

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,6 +1,6 @@
-def nickname_as (user, pass)
-  visit nickname_path
-  fill_in "Логин", :with => user 
+def login_as(user, pass)
+  visit login_path
+  fill_in "Ник", :with => user 
   fill_in "Пароль", :with => pass
   click_button "Войти"
   response.should contain "Вы вошли на сайт"
@@ -8,19 +8,19 @@ end
 
 Допустим /^я залогинился как (.+)$/ do |nickname|
   Допустим "есть пользователь #{nickname} с паролем 123456"
-  nickname_as nickname, "123456"
+  login_as nickname, "123456"
 end
 
 Допустим /^я залогинился с ролью (.+) как (.+)$/ do |role, nickname|
   Допустим "есть пользователь #{nickname} с паролем 123456"
   User.find_by_nickname( nickname ).has_role! role
-  nickname_as nickname, "123456"
+  login_as nickname, "123456"
 end
 
 Допустим /^я разлогинен$/ do
   visit root_path
   if response.body =~ /выйти/
-    click_ "выйти"
+    click_button "выйти"
     response.should contain "Вы успешно прекратили сеанс работы!"
   end
 end
