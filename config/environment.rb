3ae28f480ea82138ea83dd165a769fe8e3131346
Vladimir Parfinenko <vladimir.parfinenko@gmail.com>
2009-11-01 23:34:39 +0600

    Переезд на rails 2.3.4

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -25,7 +25,7 @@ Rails::Initializer.run do |config|
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
   # config.gem "bj"
   config.gem "authlogic", :version => ">=2.1.1"
-  config.gem "yaroslav-russian", :lib => "russian", :source => "http://gems.github.com"
+  config.gem "yaroslav-russian", :version => ">=0.2.2", :lib => "russian", :source => "http://gems.github.com"
   config.gem "be9-acl9", :source => "http://gems.github.com", :lib => "acl9"
   config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
   config.gem "webrat", :version => ">=0.4.3"
